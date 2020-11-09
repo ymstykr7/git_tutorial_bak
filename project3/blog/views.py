f5ec1492c0598dfa28d3348d3f53c19da0826303
ymstykr7 <ymstykr7@gmail.com>
2020-11-09 11:44:31 +0900

    commit

--- project3/blog/views.py
+++ project3/blog/views.py
@@ -1,7 +1,9 @@
-from django.views import generic
-from .models import Post, Category
 from django.db.models import Q
-from django.shortcuts import get_object_or_404
+from django.views import generic
+from .models import Post, Category, Comment
+from django.db.models import Q
+from .forms import CommentCreateForm
+from django.shortcuts import get_object_or_404, redirect
 
 class IndexView(generic.ListView):
     model = Post
@@ -28,3 +30,14 @@ class CategoryView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model = Post
+
+class CommentView(generic.CreateView):
+    model = Comment
+    form_class = CommentCreateForm
+
+    def form_valid(self, form):
+        post_pk = self.kwargs['post_pk']
+        comment = form.save(commit=False)
+        comment.post = get_object_or_404(Post, pk=post_pk)
+        comment.save()
+        return redirect('blog:detail', pk=post_pk)
